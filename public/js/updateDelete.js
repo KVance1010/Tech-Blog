f686887bd3ae25532c0fc1b1b537872810252e6a
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-07 23:02:12 -0800

    added update and delete to handlebars

---
+++ public/js/updateDelete.js
@@ -0,0 +1,17 @@
+// QuerySelectors
+const deleteData = document.querySelector('#delete');
+const updateData = document.querySelector('#update');
+
+// This function handles the logging in
+const deleteHandler = async () => {
+ 
+};
+
+// This function handles the Signing up
+const updateHandler = async () => {
+
+};
+
+// Event listeners
+updateData.addEventListener('click', updateHandler);
+deleteData.addEventListener('click', deleteHandler);
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,3 @@
 module.exports = {
-    format_date: (date) => {return date.toLocaleDateString();},
+    format_date: (date) => {return date.toLocaleDateString()},
 };
