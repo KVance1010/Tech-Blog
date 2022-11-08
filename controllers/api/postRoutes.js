9d7277b7d1ab9482a5212022b85713f19066d083
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-08 09:58:39 -0800

    created a delete route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -7,11 +7,29 @@ router.post('/', auth, async (req, res) => {
 		const createComment = await Post.create({
 			title: req.body.blogTitle,
 			post: req.body.blogPost,
-			user_id: req.session.userId
+			user_id: req.session.userId,
 		});
 		res.status(200).json(createComment);
 	} catch (err) {
 		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+router.delete('/:id', auth, async (req, res) => {
+	try {
+		const deletedPost = await Project.destroy({
+			where: {
+				id: req.params.id
+			},
+		});
+
+		if (!deletedPost) {
+			res.status(404).json({ message: 'No blog found with this id!' });
+			return;
+		}
+		res.status(200).json(deletedPost);
+	} catch (err) {
 		res.status(500).json(err);
 	}
 });
