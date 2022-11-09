23d2d151c8ef6c56a2aba06bf7a322ea7a2a619f
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-08 20:32:37 -0800

    moved login session id

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -35,15 +35,15 @@ router.post('/signup', async (req, res) => {
         return;
       }
       
-      req.session.userId = userData.id;
+      
       const validPassword = await userData.checkPassword(req.body.password);
-  
       if (!validPassword) {
         res
           .status(400)
           .json({ message: 'Incorrect User ID or Password. Please try again!' });
         return;
       }
+      req.session.userId = userData.id;
       req.session.save(() => {
         req.session.loggedIn = true;
         res
